3781805cd8a2ba9b3cb65390af1d494a84aec055
royeradames <royeraadames@gmail.com>
2021-08-27 20:38:12 -0400

    add form html

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -24,5 +24,54 @@ export default function Form(props) {
 
   const [emailList, setEmailList] = useState(emails)
 
-  return <div>Form</div>
+  return (
+    <div>
+      Form
+      <TextField
+        id="outlined-basic"
+        label="First Name"
+        variant="outlined"
+        value={firstName}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Last Name"
+        variant="outlined"
+        value={lastName}
+      />
+      <article className="form__emails">
+        <h2 className="form__title">email</h2>
+
+        {unpackgeEmails()}
+
+        {/* add email */}
+        <article
+          className="form__add-email"
+          onClick={() => {
+            // todo: on click add a specify email
+            console.log("cliked add email")
+          }}
+        >
+          <Add className="form__add-email-icon" />
+          <p className="form__add-email-label">Add email</p>
+        </article>
+      </article>
+      <Button
+        variant="contained"
+        color="secondary"
+        className="form__delete"
+        onClick={() => {
+          setEmailList(["on", "button", "click"])
+        }}
+      >
+        Delete
+      </Button>
+      <Button variant="outlined" color="secondary" className="form__cancel">
+        Cancel
+      </Button>
+      <Button variant="contained" color="primary" className="form__save">
+        Save
+      </Button>
+    </div>
+  )
 }
