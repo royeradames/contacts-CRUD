78e076d4b3191b3bb08c5f6ed2877d46fd48d5cb
royeradames <royeraadames@gmail.com>
2021-08-27 20:28:06 -0400

    📦import libraries and components

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+/* libraries */
+import React, { useState } from "react"
+
+/* components */
+import Contacts from "../components/Contacts"
+import Form from "../components/Form"
+import SEO from "../components/SEO"
+
+export default function Home() {
+  return <main></main>
+}
