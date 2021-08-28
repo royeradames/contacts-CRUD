77e8e654b0cea0c6f96e7aeec0f97ecfceb27439
royeradames <royeraadames@gmail.com>
2021-08-27 20:18:57 -0400

    📦add plugins and metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,44 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "avb contacts",
+    description:
+      "A contact application that reads, creates, updates, and deletes contacts. ",
+    author: "Royer Adames",
+    repo: "https://github.com/royeradames/contacts-CRUD",
+    socialLinks: {
+      blog: "https://royeraadames.medium.com/",
+      github: "https://github.com/royeradames",
+      linkedIn: "https://www.linkedin.com/in/royer-adames/",
+    },
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `components`,
+        path: `${__dirname}/src/components/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
 }
