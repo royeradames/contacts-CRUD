5f078816668e16b7bd459c0e39578754cf20be2b
royeradames <royeraadames@gmail.com>
2021-08-27 20:25:14 -0400

    📦define types

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -5,6 +5,18 @@ import axios from "axios"
 /* SVGs */
 import Add from "../assets/plug-sign.svg"
 
+/* types */
+type Contact = {
+  id: number
+  firstName: string
+  lastName: string
+  email: string[] | []
+}
+type noContacts = {
+  firstName: string
+  lastName: string
+}
+type ContactList = Contact[] | noContacts[]
 export default function Contacts(props) {
   return <article></article>
 }
