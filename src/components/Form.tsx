8b3e1aa1f56a03be1933269eb45f66d90d63d8ec
royeradames <royeraadames@gmail.com>
2021-08-27 20:36:53 -0400

    📦import libraries and assets

---
+++ src/components/Form.tsx
@@ -0,0 +1,14 @@
+/* libraries */
+import React, { useEffect, useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import TextField from "@material-ui/core/TextField"
+
+/* SVGs */
+import Add from "../assets/plug-sign.svg"
+import Substract from "../assets/minus-button.svg"
+import axios from "axios"
+import { Button } from "@material-ui/core"
+
+export default function Form(props) {
+  return <div>Form</div>
+}
