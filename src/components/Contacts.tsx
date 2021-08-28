286b1112620a6cdd3a23732876f093883cdb2fb9
royeradames <royeraadames@gmail.com>
2021-08-27 20:24:57 -0400

    📦import libaries and assets

---
+++ src/components/Contacts.tsx
@@ -0,0 +1,10 @@
+/* libraries */
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
+/* SVGs */
+import Add from "../assets/plug-sign.svg"
+
+export default function Contacts(props) {
+  return <article></article>
+}
