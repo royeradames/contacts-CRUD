384bf1725ba1ae71ff4765b8d1f2bcb4ac21e66a
royeradames <royeraadames@gmail.com>
2021-08-27 20:37:14 -0400

    📦declare types

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,7 +8,17 @@ import Add from "../assets/plug-sign.svg"
 import Substract from "../assets/minus-button.svg"
 import axios from "axios"
 import { Button } from "@material-ui/core"
-
+type Contact = {
+  id: string
+  firstName: string
+  lastName: string
+  emails: [] | string[]
+}
+type Error = {
+  statusCode: number
+  message: string
+  error: string
+}
 export default function Form(props) {
   return <div>Form</div>
 }
