920b82fc06603c542819353664a74b7170b549e6
royeradames <royeraadames@gmail.com>
2021-09-02 15:49:23 -0400

    📦import libraries and innit form

---
+++ src/components/form/index.tsx
@@ -0,0 +1,11 @@
+/* libraries */
+import React, { useEffect, useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import TextField from "@material-ui/core/TextField"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+export default function Form(props) {
+  return <></>
+}
