114f23072867d19d3726ef4aa50e9b26bb809c73
royeradames <royeraadames@gmail.com>
2021-09-02 16:26:24 -0400

    📦import assets

--- src/components/form/NewEmails.tsx
+++ src/components/form/NewEmails.tsx
@@ -2,6 +2,10 @@
 import React, { useEffect } from "react"
 import { useForm } from "react-hook-form"
 
+/* SVGs */
+import Add from "../../assets/plug-sign.svg"
+import { useState } from "react"
+
 export default function NewEmail({ setEmailList, emailList }) {
   return <></>
 }
