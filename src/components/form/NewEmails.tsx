364316fc359deb0ef874e9bac16bdc3577b8a033
royeradames <royeraadames@gmail.com>
2021-09-02 16:28:50 -0400

    📦on submit add email to the email list and don't allow duplicates

--- src/components/form/NewEmails.tsx
+++ src/components/form/NewEmails.tsx
@@ -22,5 +22,27 @@ export default function NewEmail({ setEmailList, emailList }) {
   const innitError = { type: "", message: "" }
   const [error, setError] = useState(innitError)
 
+  /* save email */
+  const onSubmit = (data: IFormInputs) => {
+    const { email } = data
+
+    const notDuplicate = emailList.indexOf(email) === -1 ? true : false
+
+    /* add to the unsave email list */
+    // don't allow duplicates
+    if (notDuplicate) {
+      setEmailList([...emailList, email])
+
+      /* reset text field */
+      setValue("email", "")
+      setError(innitError)
+    } else {
+      /* add an error message */
+      setError({
+        type: "duplicate",
+        message: "This email is all ready on the list.",
+      })
+    }
+  }
   return <></>
 }
