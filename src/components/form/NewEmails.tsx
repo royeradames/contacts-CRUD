67086d09a25806b353d293b09b49c7cf27a4cc46
royeradames <royeraadames@gmail.com>
2021-09-02 16:25:47 -0400

    📦import library

--- src/components/form/NewEmails.tsx
+++ src/components/form/NewEmails.tsx
@@ -1,5 +1,6 @@
 /* libraries */
 import React, { useEffect } from "react"
+import { useForm } from "react-hook-form"
 
 export default function NewEmail({ setEmailList, emailList }) {
   return <></>
