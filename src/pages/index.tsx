56a1170f892de2704b5fa0087ca2255d5196859b
royeradames <royeraadames@gmail.com>
2021-09-02 16:52:03 -0400

    📦move the form types in page/index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,31 @@ import React, { useState } from "react"
 
 /* components */
 import Contacts from "../components/Contacts"
-import Form from "../components/Form"
+import Form from "../components/form/index"
 import SEO from "../components/SEO"
+
+/* styles */
+import "../styles/main.scss"
+
+/* types */
+export type Contact = {
+  id: string
+  firstName: string
+  lastName: string
+  emails: string[] | []
+}
+export type noContacts = {
+  firstName: string
+  lastName: string
+}
+export type ContactList = Contact[] | noContacts[]
 
 export default function Home() {
   const [selectedContact, setSelectedContact] = useState(false)
   const [showContactDetail, setShowContactDetail] = useState(false)
 
   return (
-    <main>
+    <main className="layout">
       <SEO title="AVB Contacts" />
       <Contacts
         setSelectedContact={setSelectedContact}
