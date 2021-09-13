add39e83623701071e9e7a55b5a4967a7d6b5476
royeradames <royeraadames@gmail.com>
2021-09-13 15:32:19 -0400

    📦create new Contact

--- cypress/integration/CRUD-functions/from_crud.spec.js
+++ cypress/integration/CRUD-functions/from_crud.spec.js
@@ -5,3 +5,37 @@ describe("load the page", () => {
     cy.get(".form")
   })
 })
+describe("create new Contact", function () {
+  it("create new contact", () => {
+    // click add new contact button
+    cy.contains(/contacts/i).click()
+
+    // fill text fields
+    cy.contains(/first name/i)
+      .parent()
+      .find("input")
+      .type("Cypress")
+    cy.contains(/last name/i)
+      .parent()
+      .find("input")
+      .type("Royer")
+
+    /* add email */
+    // add email by hitting enter key
+    cy.get("input[type=email").type("cypress@gmail.com")
+    cy.get("button[form=new-email]").click()
+    cy.contains(/first name/i)
+      .parent()
+      .find("input")
+      .should("have.value", "Cypress") // check that names fields were not deleted
+    cy.get("input[type=email").type("royer@gmail.com")
+    cy.get("button[form=new-email]").click()
+
+    // click save button
+    cy.contains(/save/i).click()
+
+    // error duplication should not appear
+    cy.contains(/contact already exist/i).should("not.exist")
+    cy.contains(/Form failed to save/i).should("not.exist")
+  })
+})
