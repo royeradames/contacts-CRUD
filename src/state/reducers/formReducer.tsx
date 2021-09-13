0de5370b015053e0a866947c514d62486a8fed1d
royeradames <royeraadames@gmail.com>
2021-09-13 15:44:46 -0400

    📦define innitial state

--- src/state/reducers/formReducer.tsx
+++ src/state/reducers/formReducer.tsx
@@ -16,3 +16,29 @@ export type FormState = {
   contactList: FormState["selectedContact"][]
   formMethods: any
 }
+/* innitial state */
+const defaultState: FormState = {
+  /* toggle */
+  // form erros messages
+  isDuplicate: false,
+  isFailSave: false,
+  isFailDelete: false,
+  // contact detail pannel
+  showContactDetail: false,
+  /* current contact details and contact name list state */
+  selectedContact: {
+    id: "",
+    firstName: "",
+    lastName: "",
+    emails: [],
+  },
+  contactList: [],
+  formMethods: {
+    getValues: function () {},
+    setValue: function () {},
+    reset: function () {},
+    register: function () {},
+    handleSubmit: function () {},
+    errors: function () {},
+  },
+}
